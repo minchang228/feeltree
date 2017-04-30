f5e42e4784d5936a18ff45b680965456da1feb44
minchang228 <minchang228@gmail.com>
2017-04-30 10:25:19 +0800

    Add files via upload

---
+++ webroot/basic_chart2.php
@@ -0,0 +1,39 @@
+<?php
+require_once("phpchart/conf.php");
+include 'adodb5/adodb.inc.php';
+$db = newADOConnection('mysqli');
+$db->connect('localhost', 'root', 'bigthinker', 'feeltree');
+$db->Execute('SET NAMES UTF8');
+
+$sql="SELECT yaw,pitch,roll from treedata WHERE sensorid=2 ORDER BY id";
+$rs=$db->Execute($sql);
+$rows=$rs->getRows();
+
+$yaw=array();
+$pitch=array();
+$roll=array();
+foreach($rows as $value)
+{
+	$yaw[]=$value['yaw']*100;
+	$pitch[]=abs($value['pitch']*100);
+	$roll[]=abs($value['roll']*100);
+}
+
+//print_r($yaw);
+
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>phpChart - Basic Chart</title>
+</head>
+<body>
+    
+<?php
+$pc = new C_PhpChartX(array($yaw,$pitch,$roll),'basic_chart');
+$pc->draw();
+?>
+
+</body>
+</html>
